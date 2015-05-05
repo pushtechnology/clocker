2bf3324a0532b5568d5e02688f71a382399e2c6d
Matt Champion <mattunderscorechampion@gmail.com>
2015-05-05 21:40:22 +0100

    Made lock final

--- docker/src/main/java/brooklyn/location/docker/DockerHostLocation.java
+++ docker/src/main/java/brooklyn/location/docker/DockerHostLocation.java
@@ -84,7 +84,7 @@ public class DockerHostLocation extends AbstractLocation implements MachineProvi
 
     private static final Logger LOG = LoggerFactory.getLogger(DockerHostLocation.class);
 
-    private transient Lock lock = new ReentrantLock();
+    private transient final Lock lock = new ReentrantLock();
 
     @SetFromFlag("machine")
     private SshMachineLocation machine;
